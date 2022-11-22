bfa3829ea811e4fc4fcaca199aa327481dc913cb
katirina233 <118421008+katirina233@users.noreply.github.com>
2022-11-22 13:42:48 +0900

    Add files via upload

---
+++ стключение.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        int[] arr = { 19, 0, 75, 52 };
+
+        try
+        {
+
+    
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] / arr[i + 1]);
+            }
+        }
+
+        catch (IndexOutOfRangeException e)
+        {
+
+            Console.WriteLine("Произошло исключение: попытка деления на ноль: {0}", e.Message);
+        }
+
+
+        catch (DivideByZeroException e)
+        {
+
+            Console.WriteLine("Произошло исключение: попытка деления на ноль : {0}", e.Message);
+        }
+
+
+        catch (ArgumentOutOfRangeException e)
+        {
+
+            Console.WriteLine("Произошло исключение: попытка деления на ноль : {0}", e.Message);
+        }
+
+    
+
+        finally
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(" {0}", arr[i]);
+            }
+        }
+    }
+}
